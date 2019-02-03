a81e360e5c4bb9b39bef5869e6a3b81692398c8d
xinyuewang1 <xinyue.wang1@ucdconnect.ie>
2019-02-03 17:39:45 +0000

    A* algo implemented.

---
+++ AStar.py
@@ -0,0 +1,106 @@
+# -----------
+# User Instructions:
+#
+# Modify the the search function so that it becomes
+# an A* search algorithm as defined in the previous
+# lectures.
+#
+# Your function should return the expanded grid
+# which shows, for each element, the count when
+# it was expanded or -1 if the element was never expanded.
+#
+# If there is no path from init to goal,
+# the function should return the string 'fail'
+# ----------
+from pprint import pprint
+import math
+
+grid = [[0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0]]
+heuristic = [[9, 8, 7, 6, 5, 4],
+             [8, 7, 6, 5, 4, 3],
+             [7, 6, 5, 4, 3, 2],
+             [6, 5, 4, 3, 2, 1],
+             [5, 4, 3, 2, 1, 0]]
+
+init = [0, 0]
+goal = [len(grid) - 1, len(grid[0]) - 1]
+cost = 1
+
+delta = [[-1, 0],  # go up
+         [0, -1],  # go left
+         [1, 0],  # go down
+         [0, 1]]  # go right
+
+delta_name = ['^', '<', 'v', '>']
+
+
+def search(grid, init, goal, cost, heuristic):
+    # ----------------------------------------
+    # modify the code below
+    # ----------------------------------------
+    # my first thought again, is dictionary. each position as key, value is list of neighbors.
+    closed = [[0 for col in range(len(grid[0]))] for row in range(len(grid))]
+    closed[init[0]][init[1]] = 1
+
+    expand = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+    # what's this action for?
+    action = [[-1 for col in range(len(grid[0]))] for row in range(len(grid))]
+
+    x = init[0]
+    y = init[1]
+    g = 0
+
+    open = [[g, x, y, heuristic[x][y]]]
+
+    found = False  # flag that is set when search is complete
+    resign = False  # flag set if we can't find expand
+    count = 0
+
+    while not found and not resign:
+        if len(open) == 0:
+            resign = True
+            return "Fail"
+        else:
+            open.sort()
+            open.reverse()
+            # print(open)
+            next = open.pop()
+            # print(open)
+            while open:
+                g, x, y, f = open.pop()
+                # expand[next[1]][next[2]] = count
+                # count += 1
+                # pprint(expand)
+                closed[x][y] = 1
+                if f < next[3]:
+                    next = [g, x, y, f]
+
+            x = next[1]
+            y = next[2]
+            g = next[0]
+            f = next[3]
+            expand[x][y] = count
+            count += 1
+            # print(next)
+
+            # the key is the open list now shall only contain unvisited neighbors of current position
+            if x == goal[0] and y == goal[1]:
+                found = True
+            else:
+                for i in range(len(delta)):
+                    x2 = x + delta[i][0]
+                    y2 = y + delta[i][1]
+                    if 0 <= x2 < len(grid) and 0 <= y2 < len(grid[0]):
+                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
+                            g2 = g + cost
+                            open.append([g2, x2, y2, g2+heuristic[x2][y2]])
+                            closed[x2][y2] = 1
+
+    return expand
+
+
+pprint(search(grid, init, goal, cost, heuristic))
